53d04fb57b1f7758371c69c6e8464cb21e94f66a
Kate <mitruskasia@gmail.com>
2020-04-13 20:10:42 +0200

    Work has begun
    I managed to create my branch and start working. I made UI widget with progress bar which is supposed to be health bar.
    I'll get back to work as soon as possible.

--- PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Mike/Documents/GitHub/PangeaRepo/PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/kociu/OneDrive/Documents/GitHub/PangeaRepo/PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
