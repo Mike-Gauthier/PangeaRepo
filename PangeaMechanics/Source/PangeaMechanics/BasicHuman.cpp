2b1640ee395f4752f57f40dc255a76c23fdbe3cd
Christopher A Cornford <ccornford02906@gmail.com>
2020-05-09 18:33:57 -0400

    Commented Code

--- PangeaMechanics/Source/PangeaMechanics/BasicHuman.cpp
+++ PangeaMechanics/Source/PangeaMechanics/BasicHuman.cpp
@@ -13,60 +13,70 @@
 
 void ABasicHuman::CheckIfOwnerIsNear()
 {
+	// if (The distance from me to my owner is more than my owners InfluenceRadius)
 	if (FVector::Dist(GetActorLocation(), MyOwner->GetActorLocation()) > MyOwner->InfluenceRadius) 
 	{
+		//Trigger the Blueprint Event
 		onOwnerMoveOutOfRange();
 	}
 }
 
 const ARecastNavMesh* ABasicHuman::GetRecastNavMesh()
 {
+	//Grab the world Navigation System
 	auto NavSystem = GetWorld()->GetNavigationSystem();
 
 	if (!NavSystem) { return NULL; }
 
+	//From the Navigation System, grab the Main Navigation Data
 	auto NavData = NavSystem->GetMainNavData();
 
 	if (!NavData) { return NULL; }
 
+	//Cast the Navigation Data into a pointer that can read the NavMesh at runtime
 	const ARecastNavMesh* NavMesh = Cast<ARecastNavMesh>(NavData);
 
 	if (!NavMesh) { return NULL; }
 
+	//Return the Map to Atlantis
 	return NavMesh;
 }
 
+//Blueprint getter for MyOwner
 AActor* ABasicHuman::GetMyOwner()
 {
 	return Cast<AActor>(MyOwner);
 }
 
+//Blueprint Getter for Distance to MyOwner
 float ABasicHuman::GetDistanceToOwner()
 {
 	FVector myLoc = GetActorLocation();
 	FVector ownerLoc = MyOwner->GetActorLocation();
 
 	return FVector::Dist(ownerLoc, myLoc);;
+
+	// TODO: Add if statement to check if owner is too far, nullifying CheckIfOwnerIsNear()
 }
 
 FVector ABasicHuman::GetRandomPointNearOwner()
 {
+	//Grab owner's location
 	FVector ownerLoc = MyOwner->GetActorLocation();
 
-	const ARecastNavMesh* navMesh = GetRecastNavMesh();
-
-	FNavLocation targetLoc;
-
-	navMesh->GetRandomReachablePointInRadius(ownerLoc, MyOwner->InfluenceRadius, targetLoc);
-
+	//Store it in a FVector that will be affected.
 	FVector finalLoc = ownerLoc;
 
-	FVector2D locationMod = FMath::RandPointInCircle(250);
+	//Grab a random point from within a circle with radius equal to MyOwner's Influence Radius
+	FVector2D locationMod = FMath::RandPointInCircle(MyOwner->InfluenceRadius);
 	
+	//Modifiers are added to the Owner's location to ensure that the center of the circle is the Owner AActor
 	finalLoc.X += locationMod.X;
 	finalLoc.Y += locationMod.Y;
 	
+	// Set the z component of the finalLoc to the Owner's Z-Axis to ensure no clipping through floors.
 	finalLoc.Z = MyOwner->GetActorLocation().Z;
 
+	//Return the finalLoc with modifiers added.
 	return finalLoc;
 }
--- PangeaMechanics/Source/PangeaMechanics/BasicHuman.h
+++ PangeaMechanics/Source/PangeaMechanics/BasicHuman.h
@@ -21,29 +21,38 @@ class PANGEAMECHANICS_API ABasicHuman : public APangeaMechanicsCharacter
 	GENERATED_BODY()
 
 public:
+
+	//Mostly a redundant variable, planning on deleting.
 	UPROPERTY(EditAnywhere, Category = "Pet Information")
 		APangeaMechanicsCharacter* MyOwner;
 
+	//AActor pointer to the player that 'owns' this creature.
 	UPROPERTY(EditAnywhere, Category = "Pet Information")
 		AActor* OwnerActor;
 	
 public:
-
+	//A blueprint event that signals the creature is now too far from it's owner.
 	UFUNCTION(BlueprintImplementableEvent)
 	void onOwnerMoveOutOfRange();
 
+	//A blueprint function that checks distance to owner, and if greater than the owners 'InfluenceRadius' triggers the onOwnerMoveOutOfRange event.
 	UFUNCTION(BlueprintCallable, Category = "Custom Navigation")
 		void CheckIfOwnerIsNear();
 
+	//Simple blueprint getter that returns the AActor* for MyOwner.
 	UFUNCTION(BlueprintGetter, Category = "Custom Navigation")
 	AActor* GetMyOwner();
 
+	// TODO: Check to see if redundant with CheckIfOwnerIsNear(), very possible to neated up.
 	UFUNCTION(BlueprintGetter, Category = "Custom Navigation")
 		float GetDistanceToOwner();
 
+	//Grabs an FVector of a random point in a circle around the player with a radius equal to the owner's 'Influence Radius'.
 	UFUNCTION(BlueprintGetter, Category = "Custom Navigation")
 		FVector GetRandomPointNearOwner();
 
+	//Utility function left over from testing, resulting ARecastNavMesh* has many powerful tools that may be helpful later so I left it in for now.
+	//Grabs a reference to a pointer that allows us to read the NavMesh at runtime.
 	const ARecastNavMesh* GetRecastNavMesh();
 
 };
