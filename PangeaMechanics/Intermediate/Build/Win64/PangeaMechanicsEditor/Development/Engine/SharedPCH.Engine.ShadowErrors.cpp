188e81d615242e62c3b7d440f10b55c8552a5048
RobinZenker <robin.zenker@gmx.de>
2020-05-18 20:27:47 +0200

    Robins Axe
    Added the Basic Axe and a Master material(WIP) for equipment along with some placeholder textures

--- PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/aleja/Documents/GitHub/PangeaRepo/PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "D:/05_Files/Github/PangeaRepo/PangeaMechanics/Intermediate/Build/Win64/PangeaMechanicsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
